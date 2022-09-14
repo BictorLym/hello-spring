4f1cc23e6aa8b0d49f48e2ce40769d62f237ff43
BictorLym <1riberty@naver.com>
2022-09-14 21:21:37 +0900

    fifth commit1

--- hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloSpringApplication {
+	//kk
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
